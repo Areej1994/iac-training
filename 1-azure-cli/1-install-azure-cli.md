9c6dedb08e2b4cbee11cc7a467c8202adf114dae
Batka, Viliam <viliam.batka@accenture.com>
2020-08-18 19:01:00 +0200

    organize folders

---
+++ 1-azure-cli/1-install-azure-cli.md
@@ -0,0 +1,29 @@
+
+# Azure CLI windows 
+* Download and install msi https://aka.ms/installazurecliwindows
+
+* run login command 
+```
+az login
+```
+
+# Azure CLI in Docker
+
+## docker ubuntu 18.04
+## https://docs.docker.com/engine/install/ubuntu/
+* Install Docker on ubuntu 18.04.
+```
+sudo apt-get install curl apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo systemctl status docker
+sudo docker run hello-world
+```
+
+* run Azure CLI in contaner
+* https://docs.microsoft.com/en-us/cli/azure/run-azure-cli-docker?view=azure-cli-latest
+```
+sudo docker run -it mcr.microsoft.com/azure-cli
+```
---
+++ 1-azure-cli/2-creste-vm.sh
@@ -0,0 +1,53 @@
+# Azure CLI Create VM in bash (use bash installed with git for windows)
+
+* https://docs.microsoft.com/en-us/cli/azure/azure-cli-vm-tutorial?tutorial-step=1&view=azure-cli-latest 
+
+az login
+az group create --name TutorialResources --location eastus
+
+az vm create --resource-group TutorialResources \
+  --name TutorialVM1 \
+  --image UbuntuLTS \
+  --generate-ssh-keys \
+  --output json \
+  --verbose
+
+#
+ssh <PUBLIC_IP_ADDRESS>
+
+# Set environment variables from CLI output
+az vm show --name TutorialVM1 --resource-group TutorialResources
+
+az vm show --name TutorialVM1 \
+  --resource-group TutorialResources \
+  --query 'networkProfile.networkInterfaces[].id' \
+  --output tsv
+
+NIC_ID=$(az vm show -n TutorialVM1 -g TutorialResources \
+--query 'networkProfile.networkInterfaces[].id' \
+-o tsv)
+
+az network nic show --ids $NIC_ID
+
+read -d '' IP_ID SUBNET_ID <<< $(az network nic show \
+  --ids $NIC_ID \
+  --query '[ipConfigurations[].publicIpAddress.id, ipConfigurations[].subnet.id]' \
+  -o tsv)
+
+VM1_IP_ADDR=$(az network public-ip show --ids $IP_ID \
+  --query ipAddress \
+  -o tsv)
+
+echo $VM1_IP_ADDR
+
+#Creating a new VM on the existing subnet
+VM2_IP_ADDR=$(az vm create -g TutorialResources \
+  -n TutorialVM2 \
+  --image UbuntuLTS \
+  --generate-ssh-keys \
+  --subnet $SUBNET_ID \
+  --query publicIpAddress \
+  -o tsv)
+
+ssh $VM2_IP_ADDR
+
