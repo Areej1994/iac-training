4d9fb310c49d59b8df5965ef6ff13fbe0fa9eec9
Batka, Viliam <viliam.batka@accenture.com>
2020-08-20 01:24:20 +0200

    reference keyvault in ARM template

--- iac-training/4-arm-templates/3-arm-advanced/1-create-multiple-resource.ps1
+++ iac-training/4-arm-templates/3-arm-advanced/1-create-multiple-resource.ps1
@@ -1,9 +1,12 @@
+# create multiple storages
+
 # https://docs.microsoft.com/en-us/azure/azure-resource-manager/templates/template-tutorial-create-multiple-instances?tabs=CLI%2Cazure-cli
 
-# Variables 
-$location = "westeurope"
-$resourceGroupName = "Skunkworks"
-$templateFileLocation = ".\iac-training\4-arm-templates\3-arm-advanced\1.1-create-multiple-resourc.json"
+# Variables
+$location = "eastus"
+$resourceGroupName = "vb-iac-taining-cli-rg"
+#$templateFileLocation = ".\iac-training\4-arm-templates\3-arm-advanced\1.1-create-multiple-resourc.json"
+$templateFileLocation = "1.1-create-multiple-resourc.json"
 
 # Create resource group and start template deployment
 New-AzResourceGroup -Name $resourceGroupName -Location "$location"
--- iac-training/4-arm-templates/3-arm-advanced/2-integrate-azure-key-vault.ps1
+++ iac-training/4-arm-templates/3-arm-advanced/2-integrate-azure-key-vault.ps1
@@ -1,26 +1,45 @@
 # https://docs.microsoft.com/en-us/azure/azure-resource-manager/templates/template-tutorial-use-key-vault
 
 # Log in to your subscription
-
-Add-AzAccount
-
+<#
+   Add-AzAccount
+#>
 # The variables
-
-$resourceGroupName = "Skunkworks" # pick something cool
-$templateFileLocation = ".\2.1-integrate-azure-key-vault.json" # this is where your template lives
-$keyVaultName = "modernengineeringkv01" # pick something unique and cool
-$objectId = "0000-0000-0000-0000" # Use Get-AzAdUser or Get-AzAdServicePrincipal to find one in your subscription
+$prefix = "vb"
+$location = "eastus"
+$resourceGroupName = "$prefix-iac-taining-cli-rg" # pick something cool
+$templateKeyVaultFileLocation = ".\2.1-integrate-azure-key-vault.json" # this is where your template lives
+$templateFileLocation = ".\2.2-reference-to-azure-key-vault.json" # this is where the keyvault is referenced
+$keyVaultName = "$prefix-modernengineeringkv02" # pick something unique and cool
+# Use Get-AzAdUser or Get-AzAdServicePrincipal to find one in your subscription
+$objectId = (Get-AzAdUser -Mail viliam.batka@accenture.com).Id  # find user by email
 
 # Create a resource group
-
-New-AzResourceGroup -Name $resourceGroupName
-
+New-AzResourceGroup -Name $resourceGroupName -Location "$location"
+yes
 # Start arm template deployment
-
+# proper pass of the cercer
 New-AzResourceGroupDeployment -Name "deploy-the-keyvault" `
                               -ResourceGroupName $resourceGroupName `
                               -Mode "Incremental" `
                               -DeploymentDebugLogLevel "All" `
-                              -TemplateFile $templateFileLocation `
+                              -TemplateFile $templateKeyVaultFileLocation `
                               -keyVaultName $keyVaultName `
                               -objectid $objectId
+
+New-AzResourceGroupDeployment -Name "reference-the-keyvault" `
+                              -ResourceGroupName $resourceGroupName `
+                              -Mode "Incremental" `
+                              -DeploymentDebugLogLevel "All" `
+                              -TemplateFile $templateFileLocation `
+                              -vaultName $keyVaultName `
+                              -secretName "thisIsASecretDontTellAnyone" `
+                              -vaultResourceGroupName $resourceGroupName
+
+
+
+
+
+<#
+
+#>
