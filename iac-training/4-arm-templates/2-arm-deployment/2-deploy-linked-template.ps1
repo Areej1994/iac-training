c68a8a1f3b1411a8455b7ba00fa4de49396d2ead
Michal Sandor <m.sandor@avanade.com>
2020-08-19 16:00:51 +0200

    Added linked templates exercise

--- iac-training/4-arm-templates/2-arm-deployment/2-deploy-linked-template.ps1
+++ iac-training/4-arm-templates/2-arm-deployment/2-deploy-linked-template.ps1
@@ -1,3 +1,47 @@
-# https://docs.microsoft.com/en-us/azure/azure-resource-manager/templates/deployment-tutorial-linked-template?tabs=azure-powershell
+$prefix = "msiac"   # This name is used to generate names for Azure resources, such as storage account name.
+$location = "eastus"
 
-!!!
+$resourceGroupName = $prefix + "-rg"
+$storageAccountName = $prefix + "store"
+$containerName = "templates"
+
+$fileName = "linkedStorageAccount.json"
+$templateFile = "2-deploy-linked-template.json"
+
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# Create a storage account
+$storageAccount = New-AzStorageAccount `
+    -ResourceGroupName $resourceGroupName `
+    -Name $storageAccountName `
+    -Location $location `
+    -SkuName "Standard_LRS"
+
+$context = $storageAccount.Context
+
+# Create a container
+New-AzStorageContainer -Name $containerName -Context $context -Permission Container
+
+# Upload the template
+Set-AzStorageBlobContent `
+    -Container $containerName `
+    -File "./$fileName" `
+    -Blob "$fileName" `
+    -Context $context
+
+
+$linkedTemplateUri = New-AzStorageBlobSASToken `
+    -Context $context `
+    -Container $containerName `
+    -Blob $fileName `
+    -Permission r `
+    -ExpiryTime (Get-Date).AddHours(2.0) `
+    -FullUri
+
+New-AzResourceGroupDeployment `
+    -Name 2-deploy-linked-template `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $templateFile `
+    -prefix $prefix `
+    -linkedTemplateUri $linkedTemplateUri `
+    -verbose
