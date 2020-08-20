a92b9afca1f8f0c647f0881757f456d7a389bebe
Batka, Viliam <viliam.batka@accenture.com>
2020-08-20 02:08:43 +0200

    adjusted parameters default values

--- iac-training/4-arm-templates/3-arm-advanced/3-vm-extensions.ps1
+++ iac-training/4-arm-templates/3-arm-advanced/3-vm-extensions.ps1
@@ -1,31 +1,28 @@
 # https://docs.microsoft.com/en-us/azure/azure-resource-manager/templates/template-tutorial-deploy-vm-extensions
 
 # Log in to Azure
-
+<#
 Add-AzAccount
+#>
 
 # Variables
-
-$resourceGroupName = "Experimental" # pick something cool
+$prefix = "vb"
+$location = "eastus"
+$resourceGroupName = "$prefix-iac-taining-cli-rg" # pick something cool
 $templateFileLocation = ".\3.1-vm-extensions.json" # this is where your template lives
-$VMName = "TheCoolestVM"
+$VMName = "TutorialVME"
 
 # Create a VM to experiment with (if none is available)
-[string]$userName = 'complexAdminUserName'
-[string]$Password = Read-Host "Type a strong password here, mind the password complexity policy" # This will be replaced in a real-life IaC scenario with a pipeline variable or keyvault value
-
-# Convert to SecureString
-[securestring]$securePassword = ConvertTo-SecureString $Password -AsPlainText -Force
-
-[pscredential]$credentialOject = New-Object System.Management.Automation.PSCredential ($userName, $securePassword)
+# Create user object
+$cred = Get-Credential -Message "Enter a username and password for the virtual machine."
 
 # Creating Resource group
-New-AzResourceGroup -Name $resourceGroupName
+New-AzResourceGroup -Name $resourceGroupName -Location "$location"
+
 # Creating VM
-New-AzVM -ResourceGroupName $resourceGroupName -Location 'westeurope' -Name $VMName -Credential $credentialOject
+New-AzVM -ResourceGroupName $resourceGroupName -Location "$location" -Name $VMName -Credential $cred -Size Standard_B2S
 
 # Launch template deployment 
-
 New-AzResourceGroupDeployment -Name deploy-extension `
                               -ResourceGroupName $resourceGroupName `
                               -Mode Incremental -DeploymentDebugLogLevel All `
